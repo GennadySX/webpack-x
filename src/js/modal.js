8b5c3f81d72b35b915fcd39d821fd66fd51025f1
Alina <alinanurislamova19@gmail.com>
2019-09-23 14:37:33 +0300

    23.09

---
+++ src/js/modal.js
@@ -0,0 +1,42 @@
+// Затемнение
+var backdrop = document.querySelector(".backdrop");
+// Затемнение
+
+// Местоположение
+var choseLocation = document.getElementById("choose-location");
+var locationForm = document.getElementById("location-form");
+var locationRadio = document.querySelector("#set-location");
+var locationCityInput = document.querySelector("#city-field");
+
+chooseLocation.onclick = function() {
+    backdrop.style.display = "block";
+    locationForm.style.display = "block";
+}
+backdrop.onclick = function() {
+    backdrop.style.display = "none";
+    locationForm.style.display = "none";
+}
+locationRadio.onchange = function (event) {
+    if (event.target.checked) {
+        locationCityInput.focus();
+    }
+}
+// Местоположение
+
+// Войти
+var loginLink = document.getElementById("login-link");
+var loginForm = document.querySelector(".login-block");
+
+loginLink.onclick = function() {
+    backdrop.style.display = "block";
+    loginForm.style.display = "block";
+}
+backdrop.onclick = function() {
+    backdrop.style.display = "none";
+    loginForm.style.display = "none";
+}
+// Войти
+
+// Регистрация
+// Регистрация
+
